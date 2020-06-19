f443d9170dd3df32b0578807020785d9fe59c085
Berkin Cetin <bcetin14@gmail.com>
2020-06-19 09:38:01 -0400

    .

---
+++ address-book/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function HomePage() {
+    return (
+        <div className ="home-page">
+            <h1>Contacts</h1>
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ address-book/src/actionCreator.js
@@ -0,0 +1,10 @@
+import { SET_CONTACTS } from './actions';
+
+const setContacts = (contacts) => {
+    return {
+        type: SET_CONTACTS,
+        payload: contacts
+    }
+};
+
+export { setContacts };
